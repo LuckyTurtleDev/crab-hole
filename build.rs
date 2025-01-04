33ce69873d5af95be6065184eb76b38a6da9f194
Dominic <git@msrd0.de>
2025-01-04 10:53:42 +0100

    add comment to build.rs that it isn't released

--- build.rs
+++ build.rs
@@ -1,3 +1,8 @@
+//! WARNING - This file is not part of the release
+//!
+//! We only use this file to emit `cfg(nightly)` which
+//! is only necessary for benchmarks.
+
 fn main() {
 	let version_meta = rustc_version::version_meta().unwrap();
 	// Enable "nightly" cfg if the current compiler is nightly.
